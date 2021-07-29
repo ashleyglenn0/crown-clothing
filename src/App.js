9e22f5387f44154bd51c8626616389fbae7d1692
Ashley <arglenn023@gmail.com>
2021-07-28 22:57:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from './Homepage.Component';
+import './Homepage.styles.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
